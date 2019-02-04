6e93453f1e44df08fe5384d7c6bb40f91fbf4205
Patrik Björn <patrik.bjorn@sigma.se>
2019-02-04 08:33:46 +0100

    Fixes issues with custom post types not being displayed with full url

--- views/partials/social-share.blade.php
+++ views/partials/social-share.blade.php
@@ -1,13 +1,13 @@
 <!-- Social -->
 <ul class="share share-social share-social-icon-md share-horizontal share-no-labels hidden-print inline-block">
     <li>
-        <a class="share-social-facebook" data-action="share-popup" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo str_replace('/wp/','/',site_url()."/".urlencode($_SERVER['REQUEST_URI'])); ?>" data-tooltip="<?php _e('Share on', 'municipio'); ?> Facebook">
+        <a class="share-social-facebook" data-action="share-popup" href="https://www.facebook.com/sharer/sharer.php?u={!! urlencode(get_permalink()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Facebook">
             <i class="pricon pricon-facebook"></i>
             <span><?php _e('Share on', 'municipio'); ?> Facebook</span>
         </a>
     </li>
     <li>
-        <a class="share-social-twitter" data-action="share-popup" href="http://twitter.com/share?url={!! urlencode(get_page_link()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Twitter">
+        <a class="share-social-twitter" data-action="share-popup" href="http://twitter.com/share?url={!! urlencode(get_permalink()) !!}" data-tooltip="<?php _e('Share on', 'municipio'); ?> Twitter">
             <i class="pricon pricon-twitter"></i>
             <span><?php _e('Share on', 'municipio'); ?> Twitter</span>
         </a>
